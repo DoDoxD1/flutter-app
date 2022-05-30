f638eb4574113a63bbb456c38060a6f49de36ae1
aunuj <aunujain53@gmail.com>
2022-05-30 17:33:42 +0530

    stateless widgets done

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,110 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    title: "Readymotive",
-    home: HomePage(),
-  ));
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Readymotive",
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        accentColor: Colors.redAccent,
+      ),
+    ),
+  );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _namecontroller = TextEditingController();
+  var myText = "Guess the name of this image";
+  @override
+  void initState(){
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Readymotive"),
       ),
-      body: Container(
-        child: Center(child: Text("Hello From ReadyMotive")),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/nature.jpg",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _namecontroller,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter name here",
+                      labelText: "Name"
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Arihant Jain"),
+              accountEmail: Text("arihantjain@readymotive.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/about.png'),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("personal details"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("aunujain53@gmail.com"),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text("Phone Numbers"),
+              subtitle: Text("8077584095"),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.save,
+        ),
       ),
     );
   }
